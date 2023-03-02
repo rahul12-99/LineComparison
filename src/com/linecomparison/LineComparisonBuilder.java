c4a0fbc127dfac1fda94851906c797270d596b71
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-02 22:15:21 +0530

    Added: Refactor code for check cartesian line equal greater or small

--- src/com/linecomparison/LineComparisonBuilder.java
+++ src/com/linecomparison/LineComparisonBuilder.java
@@ -3,48 +3,57 @@ package com.linecomparison;
 import java.util.Scanner;
 
 public class LineComparisonBuilder {
-    public int lengthCalculate(int x1,int y1, int x2, int y2){ // Calculate length and return
-        int length = (int)Math.pow((x2-x1),2) + (int)Math.pow((y2-y1),2);
-        return (int)Math.sqrt(length);
+    // Calculate length and return
+    public int lengthCalculate(int x1, int y1, int x2, int y2) {
+        int length = (int) Math.pow((x2 - x1), 2) + (int) Math.pow((y2 - y1), 2);
+        return (int) Math.sqrt(length);
     }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        LineComparisonBuilder length = new LineComparisonBuilder();
-        int x1,y1,x2,y2;
-        int x3,y3,x4,y4;
-        System.out.println("Kindly enter first coordinates x1 and y1: ");
-        x1 = input.nextInt();
-        y1 = input.nextInt();
-        System.out.println("Kindly enter second coordinates x2 and y2: ");
-        x2 = input.nextInt();
-        y2 = input.nextInt();
 
-        System.out.println("Enter coordinates of second line");
-        System.out.println("Kindly enter first coordinates x3 and y3: ");
-        x3 = input.nextInt();
-        y3 = input.nextInt();
-        System.out.println("Kindly enter second coordinates x4 and y4: ");
-        x4 = input.nextInt();
-        y4 = input.nextInt();
-
-
-        Integer line1 =  length.lengthCalculate(x1,y1,x2,y2);
-        Integer line2 =  length.lengthCalculate(x3,y3,x4,y4);
-        // Check equality of lines
-        if(line1.equals(line2)) {
+    // Check Equality and display
+    public void checkEqual(Integer line1, Integer line2) {
+        if (line1.equals(line2) == true) {
             System.out.println("Both lines are equal");
         } else {
             System.out.println("Both lines are not equal");
         }
+    }
 
-        // comparing the two line to see which one is small, equal or greater.
-        int result = line1.compareTo(line2);
-        if(result > 0){
+    public void checkComparison(Integer line1, Integer line2) {       // Check quality and display
+        if (line1.compareTo(line2) > 0) {
             System.out.println("Line 1 is greater");
-        } else if(result < 0){
+        } else if (line1.compareTo(line2) < 0) {
             System.out.println("Line 2 is greater");
         } else {
             System.out.println("Both the lines are equal");
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program ");
+        Scanner input = new Scanner(System.in);
+        LineComparisonBuilder length = new LineComparisonBuilder();
+        // coordinate of First Line
+        int x1, y1, x2, y2;
+        // Coordinate of Second Line
+        int x3, y3, x4, y4;
+        System.out.println("Please enter first coordinates x1 and y1 Of First Line: ");
+        x1 = input.nextInt();
+        y1 = input.nextInt();
+        System.out.println("Please enter second coordinates x2 and y2 Of First Line: ");
+        x2 = input.nextInt();
+        y2 = input.nextInt();
+        System.out.println("Please enter first coordinates x3 and y3 Of Second Line: ");
+        x3 = input.nextInt();
+        y3 = input.nextInt();
+        System.out.println("Please enter second coordinates x4 and y4 Of Second Line: ");
+        x4 = input.nextInt();
+        y4 = input.nextInt();
+        Integer line1 = length.lengthCalculate(x1, y1, x2, y2);
+        Integer line2 = length.lengthCalculate(x3, y3, x4, y4);
+        System.out.println("Line1: " + length.lengthCalculate(x1, y1, x2, y2));
+        System.out.println("Line2: " + length.lengthCalculate(x3, y3, x4, y4));
+        length.checkEqual(line1, line2);
+        length.checkComparison(line1, line2);
+    }
 }
+
